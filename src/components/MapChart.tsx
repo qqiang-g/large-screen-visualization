8667fca7ab44daa7f2399ad78865f89a57943a9d
qqiang-g <740407980@qq.com>
2021-05-10 22:57:10 +0800

    fix:  国疫情分布图完成

--- src/components/MapChart.tsx
+++ src/components/MapChart.tsx
@@ -2,16 +2,84 @@ import React, {useEffect, useRef} from 'react';
 import * as echarts from 'echarts';
 import { creatChartOptions } from '../shared/chartOptions';
 import {px} from '../utils';
+import chainMapJson from '../shared/chain-map.json'
+const colors = {'青海省': '#BB31F7', '甘肃省': '#15B8FD', '四川省': '#06E1EE'};
 
 const MapChart = () => {
   const divRef = useRef(null);
   useEffect(() => {
+    var myChart = echarts.init(divRef.current);
+    // @ts-ignore
+    echarts.registerMap('CN', chainMapJson);
+    myChart.setOption(creatChartOptions({
+      xAxis: {show: false},
+      yAxis: {show: false},
+      series: [
+        {
+          type: 'map',
+          mapType: 'CN', // 自定义扩展图表类型
+          data: [
+            {name: '云南省', value: 1},
+          ],
+          label: {show: false, color: 'white'},
+          itemStyle: {
+            areaColor: '#010D3D',
+            color: colors['云南省'],
+            borderColor: '#01A7F7',
+            emphasis: {
+              label: {color: 'white'},
+              areaColor: '#5470C6',
+            },
+          }
+        },
+        {
+          type: 'map',
+          mapType: 'CN', // 自定义扩展图表类型
+          data: [
+            {name: '四川省', value: 100},
+          ],
+          itemStyle: {
+            areaColor: '#010D3D',
+            color: colors['四川省'],
+            borderColor: 'yellow',
+            emphasis: {
+              label: {color: 'white'},
+              areaColor: '#5470C6',
+            },
+          }
+        },
+        {
+          type: 'map',
+          mapType: 'CN', // 自定义扩展图表类型
+          data: [
+            {name: '青海省', value: 100},
+          ],
+          itemStyle: {
+            areaColor: '#010D3D',
+            color: colors['青海省'],
+            borderColor: '#01A7F7',
+            emphasis: {
+              label: {color: 'white'},
+              areaColor: '#5470C6',
+            },
+          }
+        },]
+    }))
   }, []);
+  
 
   return (
     <div className="bordered map-chart">
       <h2>全国疫情分布图</h2>
-      <div ref={divRef} className="chart"/>
+      <div className="wrapper">
+        <div ref={divRef} className="chart"/>
+        <div className="legend bordered">
+          <span className="icon" style={{background: colors['甘肃省']}}/>云南省
+          <span className="icon" style={{background: colors['四川省']}}/>四川籍
+          <span className="icon" style={{background: colors['青海省']}}/>青海籍
+        </div>
+        <div className="notes">此地图仅显示了中国的部分区域</div>
+      </div>
     </div>
   );
 };
